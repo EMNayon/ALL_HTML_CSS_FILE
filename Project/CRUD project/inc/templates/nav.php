b5dcd961f00becabb08299207a0f719a4f889329
EM Nayon <59680846+EMNayon@users.noreply.github.com>
2021-07-11 02:13:18 +0600

    create a navigation bar

---
+++ Project/CRUD project/inc/templates/nav.php
@@ -0,0 +1,5 @@
+<p>
+    <a href="/Test/CRUD Project/index.php?task=report">All Students</a> |
+    <a href="/Test/CRUD Project/index.php?task=add">Add new Student</a> |
+    <a href="/Test/CRUD Project/index.php?task=seed">Seed</a>
+</p> 
